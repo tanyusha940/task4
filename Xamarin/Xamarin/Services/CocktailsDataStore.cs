98b4b1d1a1b749f0034b37dcc9aa235c6f42cb14
Unknown <tanyusha_shkoda@mail.ru>
2018-03-19 16:26:39 +0300

    added cocktails

--- Xamarin/Xamarin/Services/CocktailsDataStore.cs
+++ Xamarin/Xamarin/Services/CocktailsDataStore.cs
@@ -27,6 +27,7 @@ namespace Xamarin.Services
                 new Ingredients{Id = 8, NameIngredient = "Лимон"},
                 new Ingredients{Id = 9, NameIngredient = "Апельсиновая цедра"},
                 new Ingredients{Id = 10, NameIngredient = "Апельсин"},
+                new Ingredients{Id = 11, NameIngredient = "Апельсин"},
 
             };
 
@@ -36,13 +37,13 @@ namespace Xamarin.Services
                 new Prescriptions{Id = 2, IdCocktails = 1, IdIngredient = 2, AmountIngredient = 25, Ingredients = ingredientsItems[1] },
                 new Prescriptions{Id = 3, IdCocktails = 2, IdIngredient = 3, AmountIngredient = 50, Ingredients = ingredientsItems[2] },
                 new Prescriptions{Id = 4, IdCocktails = 2, IdIngredient = 4, AmountIngredient = 25, Ingredients = ingredientsItems[3] },
-                //new Prescriptions{Id = 5, IdCocktails = 3, IdIngredient = 5, AmountIngredient = 50 },
-                //new Prescriptions{Id = 6, IdCocktails = 3, IdIngredient = 6, AmountIngredient = 15 },
-                //new Prescriptions{Id = 7, IdCocktails = 3, IdIngredient = 7, AmountIngredient = 30 },
-                //new Prescriptions{Id = 8, IdCocktails = 3, IdIngredient = 8, AmountIngredient = 20 },
-                //new Prescriptions{Id = 9, IdCocktails = 4, IdIngredient = 2, AmountIngredient = 50 },
-                //new Prescriptions{Id = 10, IdCocktails = 4, IdIngredient = 9, AmountIngredient = 150 },
-                //new Prescriptions{Id = 11, IdCocktails = 4, IdIngredient = 10, AmountIngredient = 30 },
+                new Prescriptions{Id = 5, IdCocktails = 3, IdIngredient = 5, AmountIngredient = 50, Ingredients = ingredientsItems[4] },
+                new Prescriptions{Id = 6, IdCocktails = 3, IdIngredient = 6, AmountIngredient = 15 , Ingredients = ingredientsItems[5] },
+                new Prescriptions{Id = 7, IdCocktails = 3, IdIngredient = 7, AmountIngredient = 30, Ingredients = ingredientsItems[6] },
+                new Prescriptions{Id = 8, IdCocktails = 3, IdIngredient = 8, AmountIngredient = 20, Ingredients = ingredientsItems[7] },
+                new Prescriptions{Id = 9, IdCocktails = 4, IdIngredient = 2, AmountIngredient = 50, Ingredients = ingredientsItems[2] },
+                new Prescriptions{Id = 10, IdCocktails = 4, IdIngredient = 9, AmountIngredient = 150, Ingredients = ingredientsItems[9] },
+                new Prescriptions{Id = 11, IdCocktails = 4, IdIngredient = 11, AmountIngredient = 30, Ingredients = ingredientsItems[10] },
             };
 
             var mockItems = new List<Cocktails>
@@ -57,8 +58,16 @@ namespace Xamarin.Services
                     {
                         prescriptionsItem[2], prescriptionsItem[3]
                     }},
-                //new Cocktails { Id = 3, Name = "Виски сауэр", DegreesCocktail = 40, AmountCocktail = 120,ImageCocktails = "http://res.cloudinary.com/task04/image/upload/v1520681417/icon_Classic-whiskey-sour1.jpg", IsChecked = false},
-                //new Cocktails { Id = 3, Name = "Отвертка", DegreesCocktail = 40, AmountCocktail = 120,ImageCocktails = "http://res.cloudinary.com/task04/image/upload/v1520681415/icon__DDE6586.jpg", IsChecked = false },
+                new Cocktails { Id = 3, Name = "Виски сауэр", DegreesCocktail = 40, AmountCocktail = 120,ImageCocktails = "http://res.cloudinary.com/task04/image/upload/v1520681417/icon_Classic-whiskey-sour1.jpg", IsChecked = false,
+                Prescriptions = new List<Prescriptions>
+                {
+                    prescriptionsItem[4], prescriptionsItem[5], prescriptionsItem[6], prescriptionsItem[7]
+                }},
+                new Cocktails { Id = 4, Name = "Отвертка", DegreesCocktail = 40, AmountCocktail = 120,ImageCocktails = "http://res.cloudinary.com/task04/image/upload/v1520681415/icon__DDE6586.jpg", IsChecked = false ,
+                Prescriptions = new List<Prescriptions>
+                {
+                    prescriptionsItem[8],prescriptionsItem[9],prescriptionsItem[10]
+                }},
             };
 
             foreach (var item in mockItems)
--- Xamarin/Xamarin/Views/ItemsPage.xaml.cs
+++ Xamarin/Xamarin/Views/ItemsPage.xaml.cs
@@ -1,20 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 using Xamarin.Models;
-using Xamarin.Views;
 using Xamarin.ViewModels;
 using System.ComponentModel;
 
 namespace Xamarin.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ItemsPage : ContentPage, INotifyPropertyChanged
     {
         ItemsViewModel viewModel;
