7693cd2353294685cf2c04c2e7e1d23484573240
Unknown <tanyusha_shkoda@mail.ru>
2018-03-20 10:11:47 +0300

    added grid

--- Xamarin/Xamarin/ViewModels/ItemDetailViewModel.cs
+++ Xamarin/Xamarin/ViewModels/ItemDetailViewModel.cs
@@ -8,18 +8,7 @@ namespace Xamarin.ViewModels
     public class ItemDetailViewModel : BaseViewModel
     {
         public Cocktails Item { get; set; }
-        //public Prescriptions ItemPrescriptions { get; set; }
-        //public Cocktails Degrees { get; set; }
-        //int degrees;
-        //public int Degrees { get
-        //    {
-        //        return this.degrees;
-        //    }
-        //    set
-        //    {
-        //        SetProperty(ref degrees, value);
-        //    }
-        //}
+ 
         public int Degrees { get; set; }
         public IEnumerable<Prescriptions> Prescriptions {get;set;}
 
--- Xamarin/Xamarin/Views/ItemsPage.xaml.cs
+++ Xamarin/Xamarin/Views/ItemsPage.xaml.cs
@@ -14,39 +14,45 @@ namespace Xamarin.Views
 	public partial class ItemsPage : ContentPage, INotifyPropertyChanged
     {
         ItemsViewModel viewModel;
+        ItemDetailViewModel itemDetail;
 
         public ItemsPage()
         {
             InitializeComponent();
 
             BindingContext = viewModel = new ItemsViewModel();
+            //BindingContext = itemDetail = new ItemDetailViewModel();
         }
         //private void OnStepperValueChanged(object sender, ValueChangedEventArgs e)
         //{
         //    if (headerStepper != null)
         //        headerStepper.Text = String.Format("Будет пить: {0:F1}", e.NewValue);
         //}
-        private async void OnButtonClicked(object sender, System.EventArgs e)
+        private void OnButtonClicked(object sender, System.EventArgs e)
         {
+            
             //*Int32.Parse(step.Value.ToString())
             Button button = (Button)sender;
             //Dictionary<string, int> ing = new Dictionary<string, int>();
-            List<string> name = new List<string>();
-            List<int> amount = new List<int>();
-            int first = (picker.SelectedIndex + 1) * 2 ;
-            foreach (var x in viewModel.Items)
-            {
-                if (x.IsChecked == true)
-                {
-                    foreach(var p in x.Prescriptions)
-                    {
-                        name.Add(p.Ingredients.NameIngredient);
-                        amount.Add(p.AmountIngredient * first);
-                        //ing.Add(p.Ingredients.NameIngredient, p.AmountIngredient);
-                    }
-                }
-            }            
-            await Navigation.PushAsync(new NewItemPage(name,amount,first));
+
+            //List<string> name = new List<string>();
+            //List<int> amount = new List<int>();
+            int first = (picker.SelectedIndex + 1) * 2;
+            //foreach (var x in viewModel.Items)
+            //{
+            //    if (x.IsChecked == true)
+            //    {
+            //        foreach (var p in x.Prescriptions)
+            //        {
+            //            amount.Add(itemDetail.Item.AmountCocktail * first);
+            //            name.Add(p.Ingredients.NameIngredient);
+            //            ing.Add(p.Ingredients.NameIngredient, p.AmountIngredient);
+            //        }
+            //    }
+            //}
+            firstGrid.IsVisible = false;
+            secondGrid.IsVisible = true;
+            //await Navigation.PushAsync(new NewItemPage(name,amount,first));
         }
         void picker_SelectedIndexChanged(object sender, EventArgs e)
         {
